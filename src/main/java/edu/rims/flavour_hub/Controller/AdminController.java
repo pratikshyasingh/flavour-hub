57828476b6940ac70b0dc137e3513a33c20adc11
pratikshya <pratikshyasingh2002@gmail.com>
2025-02-01 10:05:39 +0530

    home file changes

--- src/main/java/edu/rims/flavour_hub/Controller/AdminController.java
+++ src/main/java/edu/rims/flavour_hub/Controller/AdminController.java
@@ -1,5 +1,12 @@
 package edu.rims.flavour_hub.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
-    
+     @GetMapping("/admin")
+    String adminHome() {
+        return "customer/admin/home";
+    }
 }
