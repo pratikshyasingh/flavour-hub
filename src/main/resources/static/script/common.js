efaca3c312f53627386a8070747105be1b7f23e3
rupalika <rupalikanayak48@gmail.com>
2025-02-11 20:21:21 +0530

    file change

--- src/main/resources/static/script/common.js
+++ src/main/resources/static/script/common.js
@@ -7,3 +7,17 @@ function submitFeedback() {
 }
 
 
+window.addEventListener('scroll', function () {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.remove('transparent');
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+        navbar.classList.add('transparent');
+    }
+});
+
+function openLoginPage() {
+    window.location.href = 'login.html';
+}
