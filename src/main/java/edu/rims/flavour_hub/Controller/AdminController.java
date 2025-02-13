9a4d7bdf47a3dd2b05e5ffdea6aafdc2179e3ecb
pratikshya <pratikshyasingh2002@gmail.com>
2025-02-13 21:00:49 +0530

    order table add

--- src/main/java/edu/rims/flavour_hub/Controller/AdminController.java
+++ src/main/java/edu/rims/flavour_hub/Controller/AdminController.java
@@ -2,11 +2,18 @@ package edu.rims.flavour_hub.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
-     @GetMapping("/admin")
+    @GetMapping("/admin")
     String adminHome() {
         return "customer/admin/home";
     }
+
+    @GetMapping("/admin/order")
+    String adminOrder() {
+        return "customer/admin/order";
+    }
+
 }
---
+++ src/main/resources/static/script/admin.home.js
@@ -0,0 +1,7 @@
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+
+sidebarBtn.onclick = function () {
+    sidebar.classList.toggle("active");
+
+}
