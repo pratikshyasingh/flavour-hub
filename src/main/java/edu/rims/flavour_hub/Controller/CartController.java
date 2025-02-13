9743cb9c513d6f32cfe0ec7242b02d5c4dcb6377
rupalika <rupalikanayak48@gmail.com>
2025-02-13 12:16:51 +0530

    data binding

--- src/main/java/edu/rims/flavour_hub/Controller/CartController.java
+++ src/main/java/edu/rims/flavour_hub/Controller/CartController.java
@@ -1,5 +1,12 @@
 package edu.rims.flavour_hub.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class CartController {
-    
+    @GetMapping("/customer/cart")
+    String customerCart() {
+        return "customer/cart";
+    }
 }
--- src/main/java/edu/rims/flavour_hub/Controller/ProductController.java
+++ src/main/java/edu/rims/flavour_hub/Controller/ProductController.java
@@ -1,12 +1,29 @@
 package edu.rims.flavour_hub.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.rims.flavour_hub.entity.Category;
+import edu.rims.flavour_hub.repository.CategoryRepository;
 
 @Controller
 public class ProductController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping("/customer/plp")
     String customerPlp() {
         return "customer/plp";
     }
+
+    @GetMapping("/customer/category/plp")
+    String getProductByCategoryId(@RequestParam("category") String categoryId, Model model) {
+        Category category = categoryRepository.findById(categoryId).orElseThrow();
+        model.addAttribute("category", category);
+        return "customer/plp";
+    }
+
 }
